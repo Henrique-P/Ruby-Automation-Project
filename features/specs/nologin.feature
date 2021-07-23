2e1d756c5905b22890f8a62c824e4019051d76e3
Pedro H <pedrosantana762@gmail.com>
2021-07-23 18:40:58 -0300

    Now using times instead of repeated code lines

--- features/specs/nologin.feature
+++ features/specs/nologin.feature
@@ -8,11 +8,11 @@ Cenario: Adicionar um produto ao carrinho e então o remover
 Dado que estou na pagina home
 Quando adiciono um produto ao carrinho
 E o removo do carrinho
-Então devo ver 'SHOPPING-CART SUMMARY' no carrinho
+Então devo ver 'Your shopping cart is empty.' no carrinho
 
 @test4
 Cenario: Adicionar dois produtos ao carrinho e então os remover
 Dado que estou na pagina home
 Quando adiciono dois produtos ao carrinho
 E os removo do carrinho
-Então devo ver 'SHOPPING-CART SUMMARY' no carrinho
+Então devo ver 'Your shopping cart is empty.' no carrinho
--- features/step_definitions/product_steps.rb
+++ features/step_definitions/product_steps.rb
@@ -16,7 +16,7 @@ Quando('adiciono dois produtos ao carrinho') do
   @app.product.click_on_product_1
   @app.product.add_to_cart
   @app.product.more_products
-  @app.product.cclick_on_product_2
+  @app.product.click_on_product_2
   @app.product.add_to_cart
   @app.product.checkout
   expect(@app.product.verify).to include 'Your shopping cart contains: 2 Products'
@@ -27,12 +27,12 @@ E('o removo do carrinho') do
 end
 
 E('os removo do carrinho') do
-  @app.product.delet_product
-  page.driver.browser.navigate.refresh
-  @app.product.delet_product
-  page.driver.browser.navigate.refresh
+  2.times do
+    @app.product.delet_product
+    page.driver.browser.navigate.refresh
+  end
 end
 
 Então('devo ver {string} no carrinho') do |expected_message|
-  expect(find('#cart_title').text).to eql expected_message
+  expect(find('#center_column > p').text).to eql expected_message
 end
