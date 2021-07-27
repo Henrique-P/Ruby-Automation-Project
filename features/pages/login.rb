f51cfef42325bdf55b6eac6ddc5e33e7c38eea91
Pedro H <pedrosantana762@gmail.com>
2021-07-26 23:25:58 -0300

    Added comments

--- features/pages/login.rb
+++ features/pages/login.rb
@@ -1,14 +1,14 @@
 class Login < SitePrism::Page
   set_url '/'
-  element :input_email, '#email'
+  element :input_email, '#email' #selecionando o elemento que recebe o email para login
 
   def login(email, password)
-    input_email.send_keys(email)
-    fill_in 'Password', with: password
+    input_email.send_keys(email) #preenchendo o elemento de email com o email
+    fill_in 'Password', with: password #preenchendo o campo senha com a senha
     click_button 'Sign in'
   end
 
   def return_home
-    click_on('Return to Home')
+    click_on('Return to Home') #retornando ao /index para que os produtos possam ser selecionados
   end
 end
--- features/pages/order.rb
+++ features/pages/order.rb
@@ -1,13 +1,13 @@
 class Order < SitePrism::Page
-  element :bankwire, '#HOOK_PAYMENT > div:nth-child(1) > div > p > a'
+  element :bankwire, '#HOOK_PAYMENT > div:nth-child(1) > div > p > a' #selecionado o botão para o pagamento via bankwire
 
   def shipping
-    find('#uniform-cgv').click
-    click_on('Proceed to checkout')
+    find('#uniform-cgv').click #clicando na caixa de consentimento
+    click_on('Proceed to checkout') #procedendo para o proximo passo
   end
 
   def payment
-    bankwire.click
-    click_on('I confirm my order')
+    bankwire.click #clicando na forma de pagamento bankwire
+    click_on('I confirm my order') #finalizando o pedido
   end
 end
